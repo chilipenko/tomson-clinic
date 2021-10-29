5662ba93832d5fe5ccb79d30be41f9ff8dc7535c
Arsen <arsen.dzharullaev26@gmail.com>
2021-10-29 16:54:49 +0300

    front

---
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from './img/logo.png';
+import styles from './Navigation.module.css';
+
+function Navigation() {
+  return (
+    <nav>
+      <ul className={styles.navigation}>
+        <Link to="/" className={styles.logo}><img src={logo} alt="logo" /></Link>
+        <ul className={styles['menu-list']}>
+          <Link to="/"><li>О клинике</li></Link>
+          <Link to="/"><li>Стоматалогия</li></Link>
+          <Link to="/"><li>Косметалогия</li></Link>
+          <Link to="/"><li>Детская стоматалогия</li></Link>
+          <Link to="/"><li>Отзывы</li></Link>
+          <Link to="/"><li>Контакты</li></Link>
+          <Link to="/"><li>Цены</li></Link>
+        </ul>
+        <ul className={styles['contact-list']}>
+          <Link to="/"><li>Санкт-Петербург, пр Наставников д.47к.1</li></Link>
+          <Link to="/"><li>8 800 707-87-05</li></Link>
+          <Link to="/"><li>Заказать звонок</li></Link>
+        </ul>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navigation
---
+++ frontend/src/components/СonsultationForm/СonsultationForm.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './СonsultationForm.module.css'
+
+function СonsultationForm() {
+  return (
+    <div className={styles['banner-form']}>
+      <h3>СПЕЦИАЛЬНОЕ ПРЕДЛОЖЕНИЕ!</h3>
+      <h1>КОНСУЛЬТАЦИЯ</h1>
+      <p>+ ФОТО ПРОТОКОЛ</p>
+      <p>+ ФИНАНСОВЫЙ ПЛАН</p>
+      <button>ЗАПИСАТЬСЯ НА КОНСУЛЬТАЦИЮ</button>
+    </div>
+  )
+}
+
+export default СonsultationForm;
