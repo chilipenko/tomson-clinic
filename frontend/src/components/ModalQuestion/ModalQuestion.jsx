d90461700f3b29467c73d2fa9a7d58c8a84b4b17
Vladimir <300wrongaway@mail.ru>
2021-11-04 14:11:17 +0300

    Add send message to Telegramm ModalQuestion

--- frontend/src/components/ModalQuestion/ModalQuestion.jsx
+++ frontend/src/components/ModalQuestion/ModalQuestion.jsx
@@ -1,54 +1,81 @@
-import React, {useRef} from 'react';
-import styles from './ModalQuestion.module.css';
+import React, { useRef } from "react";
+import styles from "./ModalQuestion.module.css";
 
-
-function ModalQuestion({active, setActive}) {
-
+function ModalQuestion({ active, setActive }) {
   const nameUser = useRef(null);
   const textUser = useRef(null);
   const tel = useRef(null);
 
-
-  function sendUserQuestion(){}; // тут отправляем вопрос в телегу https://api.telegram.org/2070966167:AAHu1FaEA5tWa3kuKXwBNGUgasuD_mEhMIQ/sendMessage?chat_id=@TomsonClinic_bot&parse_mode=HTML&text=Это текст для бота
-//2070966167:AAHu1FaEA5tWa3kuKXwBNGUgasuD_mEhMIQ
   const changeHandler = async (event) => {
     event.preventDefault();
-    const user = nameUser.current.value; 
-  const inputText = textUser.current.value; 
-  const telNumber = tel.current.value; 
-
-    const response = await fetch('https://api.telegram.org/bot2070966167:AAHu1FaEA5tWa3kuKXwBNGUgasuD_mEhMIQ/sendMessage?chat_id=@TomsonClinic_bot&parse_mode=HTML&text=Это текст для бота', {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      credentials: "include",
-      body: JSON.stringify({ user, inputText, telNumber })
-    });
-    const data = await response.json();
-    console.log('data', data);
-    // if (data.message) {
-    //   history.push('/');
-    //   /// заменить на history push ()
-    //   dispatch({ type: LOGIN, payload: true })
-    // } else {
-    //   alert('Неверные данные')
-    // }
+    const user = nameUser.current.value;
+    const inputText = textUser.current.value;
+    const telNumber = tel.current.value;
+    const key = "2070966167:AAHu1FaEA5tWa3kuKXwBNGUgasuD_mEhMIQ";
+    const chat_id = "-1001223671091";
+    const message = `Задан вопрос сайта:%0A - <b>Имя:</b> <i>${user}</i>%0A - <b>Телефон:</b> <i>${telNumber}</i>%0A - <b>Вопрос:</b> <i>${inputText}</i>`;
+    const url = `https://api.telegram.org/bot${key}/sendMessage?chat_id=${chat_id}&text=${message}&parse_mode=html`;
+    const reg =
+      /(\+7|8)[- _]*\(?[- _]*(\d{3}[- _]*\)?([- _]*\d){7}|\d\d[- _]*\d\d[- _]*\)?([- _]*\d){6})/g;
+    const valid = reg.test(telNumber);
+    if (user == "") {
+      alert("Как к Вам обращаться?");
+    } else if (!valid) {
+      alert("Номер телефона введен неправильно!");
+    } else if (inputText == "") {
+      alert("Задайте Ваш вопрос");
+    } else {
+      const response = await fetch(url, {
+        method: "GET",
+      });
+      const data = await response.json();
+      console.log("data", data);
+      setActive(false);
+    }
   };
-  
 
-  // console.log(prices)
   return (
-    <div className={active ? `${styles.modal} ${styles.active}` : `${styles.modal}`} onClick={() => setActive(false)}>
-      <div className={active ? `${styles['modal__content']} ${styles.active}` : `${styles['modal__content']}`} onClick={e => e.stopPropagation()}>
-      <div className="mb-3">
-      <form onSubmit={changeHandler}>
-          <input ref={nameUser} className="form-control" type="text" placeholder='Имя' />
-          <input ref={tel} className="form-control" type="tel" placeholder='Ваш номер телефона' />
-          <textarea ref={textUser} className="form-control" type="text" placeholder='Напишите Ваш вопрос' />
-        <button type="submit" className={`btn ${styles["modal_button"]}`}>Отправить</button>
-        </form>
-     </div>
+    <div
+      className={
+        active ? `${styles.modal} ${styles.active}` : `${styles.modal}`
+      }
+      onClick={() => setActive(false)}
+    >
+      <div
+        className={
+          active
+            ? `${styles["modal__content"]} ${styles.active}`
+            : `${styles["modal__content"]}`
+        }
+        onClick={(e) => e.stopPropagation()}
+      >
+        <div className="mb-3">
+          <form onSubmit={changeHandler}>
+            <input
+              ref={nameUser}
+              className="form-control"
+              type="text"
+              placeholder="Имя"
+            />
+            <input
+              ref={tel}
+              className="form-control"
+              type="tel"
+              placeholder="Ваш номер телефона"
+            />
+            <textarea
+              ref={textUser}
+              className="form-control"
+              type="text"
+              placeholder="Напишите Ваш вопрос"
+            />
+            <button type="submit" className={`btn ${styles["modal_button"]}`}>
+              Отправить
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
 }
-export default ModalQuestion
+export default ModalQuestion;
