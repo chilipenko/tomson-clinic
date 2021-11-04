bc4280d366d6507a9555a6174ff8ad81cad937a1
Arsen <arsen.dzharullaev26@gmail.com>
2021-11-04 11:38:48 +0300

    refact ychoice

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,6 +1,7 @@
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import { Provider } from 'react-redux';
-import { store } from "../../redux/store";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+// import { Provider } from 'react-redux';
+// import { store } from "../../redux/store";
+import { useSelector } from 'react-redux';
 import Navigation from "../Navigation/Navigation";
 import Slider from "../Slider/Slider";
 import QuestionBlock from "../QuestionBlock/QuestionBlock";
@@ -13,11 +14,14 @@ import LeftSideBar from "../Animation/LeftSideBar";
 import YourChoiceBlock from "../YourChoice/YourChoice"
 import OnlineTable from "../OnlineTable/OnlineTable";
 import AdminForm from "../AdminForm/AdminForm";
+import AdminTable from "../AdminTable/AdminTable";
 
 
 function App() {
+
+  const admin = useSelector(state => state.adminReducer.admin) 
+
   return (
-    <Provider store={store}>
     <Router>
       {/* <Navigation /> */}
       <LeftSideBar />
@@ -35,6 +39,14 @@ function App() {
       <Route exact path='/admin'>
         <AdminForm />
       </Route>
+      {/* {
+        admin 
+          ?(<Route exact path='/admin/table'> 
+                <AdminTable />
+            </Route>
+            )
+          :<Redirect to='/' />
+      } */}
       <Route exact path='/onlinetable'>
         <OnlineTable />
       </Route>
@@ -46,7 +58,6 @@ function App() {
       <Route exact path='/denistry/prosthetics'/>
       <Footer />
     </Router>
-    </Provider>
   );
 }
 
