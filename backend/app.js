9df82a56ad5d8265ece496e100a698eafeb951da
Alexey Chilipenko <chilipenko@gmail.com>
2021-11-04 23:41:19 +0300

    added limit 50mb to app.use

--- backend/app.js
+++ backend/app.js
@@ -39,8 +39,11 @@ const corsOptions = {
 app.use(session(sessionConfig));
 app.use(cors(corsOptions));
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+
+app.use(express.urlencoded({limit: '50mb'}));
+app.use(express.json({limit: '50mb'}));
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, '..', 'frontend', 'build')));
