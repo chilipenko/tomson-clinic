d293b0f5ed988a9e85a7ed4efcca5b9d4aa21c9e
Alexey Chilipenko <chilipenko@gmail.com>
2021-11-05 09:31:02 +0300

    added extended:true to app.use(express.urlencoded...

--- backend/app.js
+++ backend/app.js
@@ -39,10 +39,10 @@ const corsOptions = {
 app.use(session(sessionConfig));
 app.use(cors(corsOptions));
 
-// app.use(express.urlencoded({ extended: false }));
+// app.use(express.urlencoded({ extended: true }));
 // app.use(express.json());
 
-app.use(express.urlencoded({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: true}));
 app.use(express.json({limit: '50mb'}));
 
 // Serve static files from the React app
